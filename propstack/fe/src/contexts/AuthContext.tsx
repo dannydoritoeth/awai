374e8504a41f1568f1cc8c2d4635488f7e335165
dannydoritoeth <80568149+dannydoritoeth@users.noreply.github.com>
2024-12-17 16:38:48 +1000

    Update AuthContext.tsx

--- propstack/fe/src/contexts/AuthContext.tsx
+++ propstack/fe/src/contexts/AuthContext.tsx
@@ -49,7 +49,11 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`
+          redirectTo: 'https://iamsccvlyhmdnkawuich.supabase.co/auth/v1/callback',
+          queryParams: {
+            access_type: 'offline',
+            prompt: 'consent',
+          }
         }
       })
       if (error) throw error
