132f2369331c5f203fdf913c612db7acbee0af77
dannydoritoeth <80568149+dannydoritoeth@users.noreply.github.com>
2024-12-18 14:47:38 +1000

    Update layout.tsx

--- propstack/fe/src/app/layout.tsx
+++ propstack/fe/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from '@/contexts/AuthContext'
+import Script from 'next/script'
 
 const inter = Inter({ 
   subsets: ["latin"],
@@ -23,6 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <Script
+          id="google-maps"
+          strategy="beforeInteractive"
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places,geometry`}
+        />
+      </head>
       <body className="bg-white">
         <AuthProvider>
           {children}
