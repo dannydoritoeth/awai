0452e4a34d80e3f75097cbbdbf87b64cccc64511
dannydoritoeth <80568149+dannydoritoeth@users.noreply.github.com>
2025-03-14 19:45:56 +1000

    updates

--- sales-copilot/api/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Generic OAuth routes that will work for multiple integrations
-router.get('/:integration/connect', authController.initiateOAuth);
-router.get('/:integration/callback', authController.handleOAuthCallback);
-
-module.exports = router; 
--- sales-copilot/api/src/routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/searchController');
-
-router.post('/query', searchController.query);
-
-module.exports = router; 
